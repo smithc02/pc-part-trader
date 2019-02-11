f60c1bb4d2d01dccedc4d730789c116cc12482aa
Caleb Smith <calebsupport@Calebs-MacBook-Air.local>
2019-02-11 11:28:05 -0600

    new_product, update_product, get_product, remove_product tested in postman and functional, Added req.session.user check for all products. Checks to make sure a sessionis active and someone is logged in before being able to edit items, delete, and add or see all items

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const massive = require('massive');
 const session = require('express-session');
 const { json } = require('body-parser');
 const auth = require('./authcontroller');
+const product = require('./productController');
 
 app.use(json());
 
@@ -27,10 +28,16 @@ massive(process.env.CONNECTION_STRING)
 	.catch(err => console.log(err));
 
 //user endpoints
-app.post('/api/register', auth.register); // register a new user
+app.post('/api/register', auth.register); // register a new user in db
 app.post('/api/login', auth.login); //login existing user
-app.get('/api/user', auth.get_user); // get users
+app.get('/api/user', auth.get_user); // get user logged in
 app.get('/api/logout', auth.logout); // logout session of current user
+
+//product endpoints
+app.post('/api/user/newproduct', product.new_product); // add new product to db
+app.put('/api/user/updateproduct/:id', product.update_product); //edit existing product in db
+app.get('/api/product', product.get_all_product); // get all existing products
+app.delete('/api/user/removeproduct/:id', product.remove_product); // remove prodcut from db
 
 app.listen(process.env.EXPRESS_PORT || 4000, () => {
 	console.log(`Listening on ${process.env.EXPRESS_PORT}`);
