5601c9aad82dc38277c3dd3ef62e776a02538c82
Caleb Smith <calebsupport@Calebs-MacBook-Air.local>
2019-02-14 16:28:38 -0600

    Connected socket.io from backend to front end successfully. Connected on server port of 4000. Used http.listen and not app.listen.

--- server/index.js
+++ server/index.js
@@ -6,8 +6,28 @@ const session = require('express-session');
 const { json } = require('body-parser');
 const auth = require('./authcontroller');
 const product = require('./productController');
+
+//imports for sockets
+
 // const http = require('http').Server(app);
-// const socketIO = require('socket.io')(http);
+// const io = require('socket.io')(http);
+
+// io.set('origins', '*:*');
+
+// // socket event listeners
+// io.on('connection', socket => {
+// 	console.log('User connected'), socket.emit('Hello', { Hello: 'Hello!' });
+
+// 	socket.on('disconnect', () => {
+// 		console.log('user disconnected');
+// 	});
+// 	socket.on('room', data => {
+// 		socket.join(data.room).emit('Joinedroom', data.room);
+// 	});
+// 	socket.on('leave', room => {
+// 		socket.emit('leave', room).leave(room);
+// 	});
+// });
 
 app.use(json());
 
@@ -21,6 +41,26 @@ app.use(
 		}
 	})
 );
+
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+io.set('origins', '*:*');
+
+// socket event listeners
+io.on('connection', socket => {
+	console.log('User connected'), socket.emit('Hello', { Hello: 'Hello!' });
+
+	socket.on('disconnect', () => {
+		console.log('user disconnected');
+	});
+	socket.on('room', data => {
+		socket.join(data.room).emit('Joinedroom', data.room);
+	});
+	socket.on('leave', room => {
+		socket.emit('leave', room).leave(room);
+	});
+});
 
 massive(process.env.CONNECTION_STRING)
 	.then(dbInstance => {
@@ -40,7 +80,8 @@ app.post('/api/user/newproduct', product.new_product); // add new product to db
 app.put('/api/user/updateproduct/:id', product.update_product); //edit existing product in db
 app.get('/api/product', product.get_all_product); // get all existing products
 app.delete('/api/user/removeproduct/:id', product.remove_product); // remove product from db
+app.get('/api/user/user&product', product.get_user_product);
 
-app.listen(process.env.EXPRESS_PORT || 4000, () => {
+http.listen(process.env.EXPRESS_PORT, () => {
 	console.log(`Server - Listening on ${process.env.EXPRESS_PORT}`);
 });
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './landing.css';
-// import axios from 'axios';
+import io from 'socket.io-client';
 
 class Landing extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			products: []
+			products: [],
+			socket: io()
 		};
 	}
 
+	componentDidMount() {
+		this.state.socket.on('connect', () => {
+			console.log('Hello, im connected');
+		});
+	}
 	render() {
 		return (
 			<div className="landing">
 				<header className="navBar">
 					<div className="loginLink">
 						<Link to="/register">Register</Link>
-						
 					</div>
+
 					<div className="registerLink">
 						<Link to="/login">Login</Link>
-
 					</div>
 				</header>
 
