cfe8b5a0bc7ab4f3894c0cd589186807945607e4
Caleb Smith <calebsupport@Calebs-MacBook-Air.local>
2019-02-27 15:16:17 -0600

    commented out and re started CSS for buyer Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -23,26 +23,27 @@ class Dashboard extends Component {
 			return (
 				<div>
 					{' '}{this.props.user.role === 'Buyer'
-						? <div className="dashboard">
-								<div className="dashboard-buyer-display">
-									<h1 className="buyer-product-username-container">
-										<div className="buyer-products-username">
-											{this.props.user.username}: Buyer
-										</div>
-									</h1>
-
-									<div className="buyer-product-user-home-container">
-										<div className="buyer-product-home-link-container">
-											<Link
-												className="dashboard-home-link2"
-												to="/">
-												Home
-											</Link>
+						? <div className="dashboard-landing">
+								<header className="dashboard-navBar">
+									<div>
+										<h1 className="dashboard-username-container">
+											{this.props.user.username}
+										</h1>
+									</div>
+									<div className="dashboard-link-login-box">
+										<div className="homeLink">
+											<div className="dashboard-home-link-box">
+												<Link
+													className="dashboard-home-link"
+													to="/">
+													Home
+												</Link>
+											</div>
 										</div>
 										<div>
 											<form action="/login">
 												<button
-													className="buyer-logout-button"
+													className="dashboard-logout-button"
 													onClick={() =>
 														this.props.logout()}>
 													Logout
@@ -50,43 +51,44 @@ class Dashboard extends Component {
 											</form>
 										</div>
 									</div>
-								</div>
+								</header>
+
 								<div>
 									<BuyerProducts />
 								</div>
 							</div>
 						: <div>
-								<div>
-									<div>
-										<div className="seller-products-button-container">
+								<div className="dashboard">
+									<header>
+										<div className="dashboard-products-button-container">
 											<a href="/sellerspecific">
-												<button className="seller-myproducts-button">
+												<button className="dashboard-myproducts-button">
 													My Products
 												</button>
 											</a>
 										</div>
-									</div>
-									<div className="seller-products-username-container">
-										<h1 className="seller-username">
-											{this.props.user.username}: Seller
-										</h1>
-									</div>
-									<div className="seller-logout-button-container">
-										<form action="/login">
-											<button
-												className="seller-logout-button"
-												onClick={() =>
-													this.props.logout()}>
-												Logout
-											</button>
-										</form>
-									</div>
-									<div className="parts-for-sale-container">
-										<h1> All parts for sale!</h1>
-									</div>
-								</div>
-								<div>
-									<Link to="/"> Home </Link>
+										<div className="dashboard-products-username-container">
+											<h1 className="dashboard-username">
+												{this.props.user.username}: Seller
+											</h1>
+										</div>
+										<div className="dashboard-logout-button-container">
+											<form action="/login">
+												<button
+													className="dashboard-logout-button"
+													onClick={() =>
+														this.props.logout()}>
+													Logout
+												</button>
+											</form>
+										</div>
+										<div className="parts-for-sale-container">
+											<h1> All parts for sale!</h1>
+										</div>
+										<div>
+											<Link to="/"> Home </Link>
+										</div>
+									</header>
 								</div>
 								<div>
 									<SellerProducts />
